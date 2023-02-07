c39ea52e3567fde7fdff78de88e9b13aa62c3806
JDMestanzy <jd.mestanza.rod@gmail.com>
2023-02-07 11:18:35 -0700

    updated just missing validation

---
+++ Mission04_juandm/Models/GradeCalculatorModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_juandm.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Required]
+        public int Assignment { get; set; }
+        public int GroupProject { get; set; }
+        public int Quiz { get; set; }
+        public int Midterm { get; set; }
+        public int Final { get; set; }
+
+        public int Intex { get; set; }
+
+    }
+}
